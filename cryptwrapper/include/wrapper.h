c2bbfffa4c72535e51682d5308ec2b1cfcaedaa8
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-10-26 00:11:04 +0200

    Added handshake && Debug info

--- cryptwrapper/include/wrapper.h
+++ cryptwrapper/include/wrapper.h
@@ -1,17 +1,30 @@
 #ifndef WRAPPER_H
 #define WRAPPER_H
 
+#include <stdbool.h>
+
 #include <sodium.h>
 
-#define TRANS_BUFF_SIZE 200
+#define TRANS_BUFF_SIZE 100
+
+#define DEBUG
+
+#ifdef DEBUG
+#define dbprintf(f, p...) printf(f, p)
+#define dbprint(f) printf(f)
+#else
+#define dbprintf(f, p...) ((void)0)
+#define dbprint(f) ((void)0)
+#endif
 
 static const unsigned char seed[randombytes_SEEDBYTES] = { "hellothereyoudingdong" };
 
 struct crypto_ctx {
-	unsigned char client_pk[crypto_kx_PUBLICKEYBYTES];
-	unsigned char client_sk[crypto_kx_SECRETKEYBYTES];
+  // Public and private key of the client
+	unsigned char self_pk[crypto_kx_PUBLICKEYBYTES];
+	unsigned char self_sk[crypto_kx_SECRETKEYBYTES];
 
-	unsigned char server_pk[crypto_kx_PUBLICKEYBYTES];
+	unsigned char other_pk[crypto_kx_PUBLICKEYBYTES];
 
 	unsigned char rx[crypto_kx_SESSIONKEYBYTES];
 	unsigned char tx[crypto_kx_SESSIONKEYBYTES];
@@ -22,5 +35,5 @@ struct crypto_ctx {
 
 int send_encrypted(int sock, struct crypto_ctx *con, unsigned char buff[TRANS_BUFF_SIZE]);
 int recv_encrypted(int sock, struct crypto_ctx *con, unsigned char buff[TRANS_BUFF_SIZE]);
-int keyexchange(int sock, struct crypto_ctx *con);
+int keyexchange(int sock, struct crypto_ctx *con, bool client);
 #endif
--- cryptwrapper/src/crypt.c
+++ cryptwrapper/src/crypt.c
@@ -1,6 +1,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <stdio.h>
 
 #include <sodium.h>
 
@@ -17,10 +18,11 @@ int send_encrypted(int sock, struct crypto_ctx *con,
 
 	len = send(sock, cipher_buff,
 		   TRANS_BUFF_SIZE + crypto_secretbox_MACBYTES, 0);
-
-  if (len > 0) { 
-    sodium_increment(con->nonce, sizeof con->nonce);
-  }
+  /*  */
+  /* if (len > 0) {  */
+  /*   sodium_increment(con->nonce, sizeof con->nonce); */
+  /*   dbprint("Nonce incremented!\n"); */
+  /* } */
 	return len;
 }
 
@@ -33,7 +35,7 @@ int recv_encrypted(int sock, struct crypto_ctx *con,
 	len = recv(sock, cipher_buff,
 		   TRANS_BUFF_SIZE + crypto_secretbox_MACBYTES, 0);
 	if (len < 0 && errno != EWOULDBLOCK && errno != EAGAIN) {
-		return len;
+		return -1;
 	}
 
 	if (len > 0) {
@@ -44,36 +46,86 @@ int recv_encrypted(int sock, struct crypto_ctx *con,
 			return -2;
 		}
 
-		sodium_increment(con->nonce, sizeof con->nonce);
+		/* sodium_increment(con->nonce, sizeof con->nonce); */
+    /* dbprint("Nonce incremented!\n"); */
 		return len;
 	}
 	return 0;
 }
 
-int keyexchange(int sock, struct crypto_ctx *con)
+void printkey(unsigned char input[], size_t len)
 {
-	crypto_kx_keypair(con->server_pk, con->server_pk);
+	size_t i;
+
+	for (i = 0; i < len; i++) {
+		dbprintf("%x", input[i]);
+	}
+	dbprint("\n");
+}
+
+int keyexchange(int sock, struct crypto_ctx *con, bool client)
+{
+	crypto_kx_keypair(con->self_pk, con->self_sk);
 
 	// send server public key
-	if (send(sock, con->server_pk, crypto_kx_PUBLICKEYBYTES, 0) !=
+	if (send(sock, con->self_pk, crypto_kx_PUBLICKEYBYTES, 0) !=
 	    crypto_kx_PUBLICKEYBYTES) {
 		return -1;
 	}
 
 	// get client public key
-	if (recv(sock, con->client_pk, crypto_kx_PUBLICKEYBYTES, 0) !=
+	if (recv(sock, con->other_pk, crypto_kx_PUBLICKEYBYTES, 0) !=
 	    crypto_kx_PUBLICKEYBYTES) {
 		return -2;
 	}
 
 	// compute shared session keys
-	if (crypto_kx_server_session_keys(con->rx, con->tx, con->server_pk,
-					  con->server_pk, con->client_pk)) {
-		return -3;
-	}
+	if(client) {
+    if (crypto_kx_client_session_keys(con->rx, con->tx, con->self_pk,
+              con->self_sk, con->other_pk)) {
+      return -3;
+    }
+  } else {
+    if (crypto_kx_server_session_keys(con->rx, con->tx, con->self_pk,
+              con->self_sk, con->other_pk)) {
+      return -3;
+    }
+  }
+
+  #ifdef DEBUG
+  dbprint("Self public key:");
+  printkey(con->self_pk, crypto_kx_PUBLICKEYBYTES);
+  dbprint("Self private key:");
+  printkey(con->self_sk, crypto_kx_SECRETKEYBYTES);
+
+  dbprint("Remote public key:");
+  printkey(con->other_pk, crypto_kx_PUBLICKEYBYTES);
+
+  dbprint("Session Keys:\nRx:");
+  printkey(con->rx, crypto_kx_SESSIONKEYBYTES);
+  dbprint("Tx:");
+  printkey(con->tx, crypto_kx_SESSIONKEYBYTES);
+  #endif
 
 	// send encrypted nonce with fixed_nonce
 	randombytes_buf_deterministic(con->nonce, sizeof con->nonce, seed);
+
+  // Test the connection with a handshake
+  if (send_encrypted(sock, con, "HelloWorld!") < 0) {
+    return -4;
+  }
+
+  int rv;
+  unsigned char temp[TRANS_BUFF_SIZE];
+  rv = recv_encrypted(sock, con, temp);
+  if (rv < 0) {
+    dbprintf("ding dong%d\n", rv);
+    return -5;
+  }
+  
+  if (temp[3] != 'l') {
+    return -6;
+  }
 
 	return 0;
 }
